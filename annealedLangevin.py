ald meas_grad normalization fix

annealedLangevin.py
@@ -1,7 +1,7 @@
 import torch
 from tqdm import tqdm as tqdm
 
-def ald(diffuser, config, Y, oracle, current, forward_operator, adjoint_operator, norm_operator):
+def ald(diffuser, config, Y, oracle, current, forward_operator, adjoint_operator,):
     
     step_images = []
     oracle_log = torch.zeros((config.sampling.num_steps, config.sampling.channels))
@@ -13,11 +13,8 @@ def ald(diffuser, config, Y, oracle, current, forward_operator, adjoint_operator
 
     with torch.no_grad():
         for step_idx in tqdm(range(config.sampling.num_steps)):
-            # Compute current step size and noise power
+            # Compute current step size, noise power and alpha
             current_sigma = diffuser.sigmas[step_idx + config.sampling.sigma_offset].item()
-            config.sampling.current_sigma = current_sigma
-            
-            # Compute alpha
             alpha = torch.tensor(config.sampling.step_size * (current_sigma / config.model.sigma_end) ** 2)
                 
             # Labels for diffusion model
@@ -28,30 +25,20 @@ def ald(diffuser, config, Y, oracle, current, forward_operator, adjoint_operator
             for inner_idx in range(config.sampling.steps_each):
                 # Compute score
                 current_real = torch.view_as_real(current).permute(0, 3, 1, 2)
-
-                # Get score
                 score = diffuser(current_real, labels)
                 
                 # View as complex
                 score = torch.view_as_complex(score.permute(0, 2, 3, 1).contiguous())
-                config.sampling.score = score
 
                 if (config.sampling.prior_sampling == 0):
-                    # Compute gradient for measurements in un-normalized space
-                    current_norm = current * config.sampling.norm_operator
-                    H_forw = forward_operator(current_norm) 
+                    # Compute gradient and normalize
+                    H_forw = forward_operator(current) 
                     H_adj = adjoint_operator(H_forw - Y)
+                    meas_grad = (H_adj * config.sampling.dc_boost) / (config.sampling.local_noise/2. + current_sigma ** 2)
 
-                    # Re-normalize gradient to match score model
-                    meas_grad = norm_operator(H_adj, config)
-
-                # Annealing noise
+                # Annealing noise and update
                 grad_noise = torch.sqrt(2 * alpha * config.sampling.noise_boost) * torch.randn_like(current) 
-                
-                # Apply update
-                current = current + alpha * (score - (meas_grad / config.sampling.dc_boost)) + grad_noise
-
-                # NRMSE
+                current = current + alpha * (score - meas_grad) + grad_noise
                 nrmse = (torch.sum(torch.square(torch.abs(current - oracle)), dim=(-1, -2)) / torch.sum(torch.square(torch.abs(oracle)), dim=(-1, -2)))
                 
             # Store Min NRMSE Image
@@ -65,6 +52,9 @@ def ald(diffuser, config, Y, oracle, current, forward_operator, adjoint_operator
             
             if step_idx % 100 == 0:
                 print('\nStep %d, NRMSE %3f' % (step_idx, torch.mean(nrmse)))
+                print('\nMeasurement Grad before Normalization: ' + str(round(float(torch.mean(torch.abs(H_adj))), 2)))
+                print('Measurement Grad after Normalization: ' + str(round(float(torch.mean(torch.abs(meas_grad))), 2)))
+                print('Score Power: ' + str(round(float(torch.mean(torch.abs(score))), 2)))
                 step_images.append(current)
     
     return {'min_nrmse_img': min_nrmse_img, 

sampling.py
@@ -48,7 +48,10 @@ elif config.model.depth == 'medium':
 elif config.model.depth == 'low':
     diffuser = NCSNv2(config)
 
-diffuser.sigmas = config.training.sigmas
+if config.training.sigmas is not None:
+    diffuser.sigmas = config.training.sigmas
+
+config.training.sigmas = diffuser.sigmas
 diffuser = diffuser.cuda()
 # !!! Load weights
 diffuser.load_state_dict(contents['model_state']) 
@@ -83,24 +86,24 @@ if not os.path.isdir(result_dir):
     os.makedirs(result_dir)
 
 forward_model = None
-Y, oracle, forward_operator, adjoint_operator, norm_operator = None, None, None, None, None
+Y, oracle, forward_operator, adjoint_operator = None, None, None, None
 
 print('Dataset: ' + config.data.file)
 print('Dataloader: ' + config.data.dataloader)
+print('\nStep Size: ' + str(np.float64(config.sampling.step_size)) + '\n') 
+best_images = []
 
 if config.sampling.prior_sampling == 0:
     print('Forward Class: ' + config.sampling.forward_class)
     forward_model = globals()[config.sampling.forward_class]()
-    Y, oracle, forward_operator, adjoint_operator, norm_operator = forward_model.DataLoader(config)
-    config.sampling.oracle_shape = oracle.shape
+    Y, oracle, forward_operator, adjoint_operator = forward_model.DataLoader(config)
+    config.sampling.channels = oracle.shape[0]
+    init_val_X = torch.randn_like(oracle).cuda()
 
-print('\nStep Size: ' + str(np.float64(config.sampling.step_size)) + '\n') 
-real = torch.randn(config.sampling.channels, config.sampling.oracle_shape[1], config.sampling.oracle_shape[2], dtype = torch.float)
-imag = torch.randn(config.sampling.channels, config.sampling.oracle_shape[1], config.sampling.oracle_shape[2], dtype = torch.float)
-init_val_X = torch.complex(real, imag).cuda()
-best_images = []
-
-if config.sampling.prior_sampling == 1:
+else:
+    real = torch.randn(config.sampling.channels, config.sampling.oracle_shape[0], config.sampling.oracle_shape[1], dtype = torch.float)
+    imag = torch.randn(config.sampling.channels, config.sampling.oracle_shape[0], config.sampling.oracle_shape[1], dtype = torch.float)
+    init_val_X = torch.complex(real, imag).cuda()
     config.sampling.noise_range = [1]
     config.sampling.noise_boost = 1
     oracle = init_val_X.clone()
@@ -108,14 +111,11 @@ if config.sampling.prior_sampling == 1:
     config.sampling.sampling_file = 'prior'
 
 # For each SNR value
-for snr_idx, local_noise in tqdm(enumerate(config.sampling.noise_range)):
+for snr_idx, config.sampling.local_noise in tqdm(enumerate(config.sampling.noise_range)):
+    # Starting with random noise and running Annealed Langevin Dynamics
     print('\n\nSampling for SNR Level ' + str(snr_idx) + ': ' + str(config.sampling.snr_range[snr_idx]))
-    # Starting with random noise
     current = init_val_X.clone()
-    config.sampling.local_noise = local_noise
-    
-    # Annealed Langevin Dynamics
-    best_images.append(ald(diffuser, config, Y[snr_idx], oracle, current, forward_operator, adjoint_operator, norm_operator))
+    best_images.append(ald(diffuser, config, Y[snr_idx], oracle, current, forward_operator, adjoint_operator))
         
 torch.cuda.empty_cache()
 